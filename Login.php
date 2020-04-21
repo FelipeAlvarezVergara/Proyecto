70f96a5f0d0afbb4cfa405dafa076e80c71aa37a
FelipeAlvarezVergara <felipe.zero.15@gmail.com>
2020-04-20 21:42:03 -0400

    Update Login.php

--- Login.php
+++ Login.php
@@ -27,3 +27,18 @@ else if($nr == 0)
 }
 
 ?>
+
+<!DOCTYPE html>
+	<html>
+	     <head>
+	     </head>
+	     <body>
+	          <h1>LINKS</h1>
+	          <h3>Lista</h3>
+	          <ul>
+	          	<li><a href="https://www.offidocs.com/index.php/desktop-programming-tools-online/eclipse-online-ide-java-c-php">Opcion 1</li>
+	          	<li><a href="https://colab.research.google.com/notebooks/intro.ipynb">Opcion 2</li>
+	          	<li><a href="https://edu.google.com/intl/es-419/products/classroom/?modal_active=none">Opcion 3</li>	
+	          </ul>
+	     </body>
+	</html>
