a3784ffdcb5d863c3964beeea054310a901f0d11
FelipeAlvarezVergara <felipe.zero.15@gmail.com>
2020-05-11 19:53:42 -0400

    HU 15, 16 y 17

---
+++ views/programador.view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Programador</title>
+  <link rel="stylesheet" href="css/provista.css">
+</head>
+<body>
+
+  <h1>LINKS DE IDE ONLINE</h1>
+      <table style="width: 300px">
+          <tr>
+            <th>#</th>
+            <th>LISTA</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td><a href="https://www.offidocs.com/index.php/desktop-programming-tools-online/eclipse-online-ide-java-c-php" target="_blank">Eclipse Online</a></td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td><a href="https://colab.research.google.com/notebooks/intro.ipynb" target="_blank">Google Colaboratory</a></td>
+          </tr>
+      </table>
+
+</body>
+</html>
