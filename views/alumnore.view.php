49b329261bc3c7a089f924ece649ba443c8644ef
FelipeAlvarezVergara <felipe.zero.15@gmail.com>
2020-07-02 12:18:16 -0400

    HU 5, 6 y 9

--- views/alumnore.view.php
+++ views/alumnore.view.php
@@ -15,7 +15,26 @@
               <div class="registro">
                   <input type="text" name="nombre" placeholder="Ingrese su Nombre" class="input-48">
                   <input type="text" name="apellidos" placeholder="Ingrese sus Apellidos" class="input-48">
-                  <input type="text" name="rut" placeholder="Ingrese su RUT" class="input-100">
+                  <input type="text" name="rut" id="rut" maxlength="12" placeholder="Ingrese su RUT" class="input-100">
+                  <script type="text/javascript">
+                        document.getElementById('rut').addEventListener('input', function(evt) {
+                        let value = this.value.replace(/\./g, '').replace('-', '');
+
+                        if (value.match(/^(\d{2})(\d{3}){2}(\w{1})$/)) {
+                        value = value.replace(/^(\d{2})(\d{3})(\d{3})(\w{1})$/, '$1.$2.$3-$4');
+                        }
+                        else if (value.match(/^(\d)(\d{3}){2}(\w{0,1})$/)) {
+                        value = value.replace(/^(\d)(\d{3})(\d{3})(\w{0,1})$/, '$1.$2.$3-$4');
+                        }
+                        else if (value.match(/^(\d)(\d{3})(\d{0,2})$/)) {
+                        value = value.replace(/^(\d)(\d{3})(\d{0,2})$/, '$1.$2.$3');
+                        }
+                        else if (value.match(/^(\d)(\d{0,2})$/)) {
+                        value = value.replace(/^(\d)(\d{0,2})$/, '$1.$2');
+                        }
+                        this.value = value;
+                      });
+                  </script>
                 <select class="form-control" name="cursos">
                   <option value="">Curso</option>
                   <option value="Algoritmos y Estrucuras de Datos">Algoritmos y Estructuras de datos</option>
